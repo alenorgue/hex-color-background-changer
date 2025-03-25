134e8ba07216f505373045792e99c550fa014102
alenorgue <alenoriegag@gmail.com>
2025-03-25 20:41:31 +0100

    randomColor taken from CGPT

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 // Implementar solución aquí
 
+const btn = document.getElementById("btn"); 
+const body = document.querySelector("body");
+const hexValue = document.getElementById("hex-value");
+
+// Función para generar color aleatorio
+const randomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+
+function handleClick() {
+    console.log("Cambiando color...");
+    body.style.backgroundColor = randomColor(); 
+    hexValue.textContent = randomColor();
+}
+btn.addEventListener("click", handleClick);
